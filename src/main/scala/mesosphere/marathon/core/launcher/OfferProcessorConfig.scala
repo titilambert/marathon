f6d2eb484bbc634d545eea982baf45cbb2d30895
Peter Kolloch <peter@mesosphere.io>
2015-09-08 15:54:32 +0200

    Save tasks shortly before launches

--- src/main/scala/mesosphere/marathon/core/launcher/OfferProcessorConfig.scala
+++ src/main/scala/mesosphere/marathon/core/launcher/OfferProcessorConfig.scala
@@ -9,6 +9,11 @@ trait OfferProcessorConfig extends ScallopConf {
     descr = "Offer matching timeout (ms). Stop trying to match additional tasks for this offer after this time.",
     default = Some(1000))
 
+  lazy val saveTasksToLaunchTimeout = opt[Int]("save_tasks_to_launch_timeout",
+    descr = "Timeout (ms) for saving all tasks for one offer that we are about to launch.",
+    default = Some(3000),
+    hidden = true)
+
   lazy val declineOfferDuration = opt[Long]("decline_offer_duration",
     descr = "(Default: 120 seconds) " +
       "The duration (milliseconds) for which to decline offers by default",
